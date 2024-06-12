284c0b74197c3e85aefbdc9c8435c6fd06d749ce
alidurgun <ali.durgun1994@gmail.com>
2024-06-12 17:54:32 +0200

    Update Workshop.cpp

--- Workshop.cpp
+++ Workshop.cpp
@@ -62,23 +62,3 @@ int CalculateMaxWorkshops(Available_Workshops* ptr)
     }
     return attendedWorkshops;
 }
-
-int main(int argc, char* argv[]) {
-    int n; // number of workshops
-    cin >> n;
-    // create arrays of unknown size n
-    int* start_time = new int[n];
-    int* duration = new int[n];
-
-    for (int i = 0; i < n; i++) {
-        cin >> start_time[i];
-    }
-    for (int i = 0; i < n; i++) {
-        cin >> duration[i];
-    }
-
-    Available_Workshops* ptr;
-    ptr = initialize(start_time, duration, n);
-    cout << CalculateMaxWorkshops(ptr) << endl;
-    return 0;
-}
